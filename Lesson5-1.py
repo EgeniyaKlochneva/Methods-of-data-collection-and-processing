e0b6f1468f9f8a62321b8b19eeced970171a4109
Klochneva <kalina7372@gmaiil.com>
2021-05-30 15:49:55 +0300

    Переделанное задание 5-1

--- Lesson5-1.py
+++ Lesson5-1.py
@@ -1,58 +1,67 @@
+from pprint import pprint
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 import time
+from selenium.webdriver.common.action_chains import ActionChains
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
-db = client['mail']
-mails = db.mails
 
-chrome_options = Options()
-chrome_options.add_argument('start-max')
+db = client['mails']
+main_coll = db.main_coll
 
 driver = webdriver.Chrome()
-driver.get('https://passport.yandex.ru/auth?from=mail&origin=hostroot_homer_auth_ru&retpath=https%3A%2F%2Fmail.yandex.ru%2F&backpath=https%3A%2F%2Fmail.yandex.ru%3Fnoretpath%3D1')
+driver.get('https://mail.ru/')
 
+# авторизация
+login = driver.find_element_by_name("login")
+login.send_keys('study.ai_172')
+button = driver.find_element_by_xpath("//button[@data-testid='enter-password']")
+button.click()
 
-assert "Авторизация" in driver.title
-elem = driver.find_element_by_id('passp-field-login')
-elem.send_keys('evgeniya.klochneva')
-elem.send_keys(Keys.RETURN)
-time.sleep(2)
-elem = driver.find_element_by_id('passp-field-passwd')
-elem.send_keys('6,Bvxk!GAHF#dJB')
-elem.send_keys(Keys.ENTER)
-time.sleep(3)
+time.sleep(1)
+password = driver.find_element_by_name("password")
+password.send_keys('NextPassword172!')
 
-# WebDriverWait(driver, 10).until(
-#              EC.element_to_be_clickable((By.CLASS_NAME, 'ns-view-messages ns-view-id-132 js-action-context')))
+button = driver.find_element_by_xpath("//button[@data-testid='login-to-mail']")
+button.click()
 
-#assert "Входящие" in driver.title
-i = 0
+# ссылки на письма
+time.sleep(5)
+mail_all = driver.find_elements_by_class_name('js-tooltip-direction_letter-bottom')
+url = [el.get_attribute('href') for el in mail_all]
+links = set(url)
+len_check = len(links)
+
 while True:
-    try:
-        dict_of_data = {}
-        froms = driver.find_elements_by_class_name('mail-MessageSnippet-FromText')
-        froms[i].click()
-        time.sleep(2)
-        sender = driver.find_element_by_class_name('mail-MessageSnippet-FromText').get_attribute('title')
-        date = driver.find_element_by_class_name('mail-MessageSnippet-Item_dateText').text
-        subject = driver.find_element_by_class_name('mail-MessageSnippet-Item mail-MessageSnippet-Item_subject').text
-        driver.back()
-        time.sleep(2)
-        dict_of_data['sender'] = sender
-        dict_of_data['date'] = date
-        dict_of_data['subject'] = subject
-        mails.insert_one(dict_of_data)
-        i += 1
-    except Exception as e:
-        print(e)
+    actions = ActionChains(driver)
+    actions.move_to_element(mail_all[-1])
+    actions.perform()
+    time.sleep(5)
+    mail_all = driver.find_elements_by_class_name('js-tooltip-direction_letter-bottom')
+    url = [el.get_attribute('href') for el in mail_all]
+    links.update(set(url))
+    if len_check == len(links):
         break
+    len_check = len(links)
 
-#ошибки Message: no such element: Unable to locate element: {"method":"css selector","selector":".mail-MessageSnippet-Item mail-MessageSnippet-Item_subject"}
-  #(Session info: chrome=90.0.4430.212)
+# данные из писем
+mails = []
+for link in links:
+    mail_data = {}
+    driver.get(link)
+    time.sleep(2)
+
+    mail_data['from'] = driver.find_element_by_class_name('letter-contact').get_attribute('title')
+    mail_data['title'] = driver.find_element_by_xpath('//h2').text
+    mail_data['text'] = driver.find_element_by_class_name('letter-body').text
+    mail_data['date'] = driver.find_element_by_class_name('letter__date').text
+    mail_data['link'] = link
+
+    mails.append(mail_data)
+
+
+for el in mails:
+    main_coll.update_one({'link': el.get('link')}, {'$set': el}, upsert=True)
+
+print(f'Писем в базе: {main_coll.count_documents({})}')
+
